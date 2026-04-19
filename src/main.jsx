4ce044ba78e34c3ef0a0392b3ca64ab536c80252
sliquemoves <rentalsutopia@gmail.com>
2026-04-19 18:04:44 -0500

    Fix iOS overscroll white background

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootEl = document.getElementById('root');
+rootEl.style.background = '#000';
+rootEl.style.minHeight = '100vh';
+ReactDOM.createRoot(rootEl).render(
   <App />
 )
