68042c8cb42a8e6dc6addc552bd5d3a31647797e
sliquemoves <rentalsutopia@gmail.com>
2026-04-19 17:42:18 -0500

    Center fleet grid vertically

--- src/components/landing/FleetSection.jsx
+++ src/components/landing/FleetSection.jsx
@@ -59,7 +59,7 @@ export default function FleetSection() {
           </h2>
         </motion.div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {vehicles.map((vehicle, index) => (
             <motion.div
               key={vehicle.name}
